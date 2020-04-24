a568542ecfd0232547b0ac99f2657a4f1ac45dae
Aleksey <aleksei.sirmais@yandex.ru>
2020-04-25 02:49:51 +0300

    test example web socket

--- src/views/createChat.js
+++ src/views/createChat.js
@@ -2,10 +2,51 @@ import ChatsTemplate from "../components/Profile/chats.pug";
 import {unSetScroll} from "../components/Desk/Desk.js";
 
 
+
+function getSomething() {
+
+
+    var socket = new WebSocket("ws://localhost:8000/echo");
+
+    socket.onopen = function () {
+        console.log("Status: Connected\n");
+    };
+
+
+    socket.onmessage = function (e) {
+        console.log("Server: " + e.data + "\n");
+    };
+
+
+    function send(msg) {
+        console.log("send webSocket:", msg)
+        socket.send(msg);
+    }
+
+
+    const btn = document.getElementById("sendBtn")
+    btn.addEventListener('click', (evt)=> {
+        send(" msg from front")
+    })
+
+    
+}
+
+
 export function CreateChatView() {
     unSetScroll();
     document.title = "Chats";
     const chats = ChatsTemplate();
     const content = document.getElementById('content');
     content.innerHTML = chats;
+
+
+
+
+    getSomething();
+
+
+
+
+
 }
