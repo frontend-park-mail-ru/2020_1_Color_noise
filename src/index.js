5f679e190e2b9500c72e7116fb0f6bcb82304ac5
Aleksey <aleksei.sirmais@yandex.ru>
2020-05-08 22:22:05 +0300

    offline

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import Router from "./utils/router.js"
 
 if ('serviceWorker' in navigator) {
     window.addEventListener("load", () => {
-
-        // 'service-worker.js'
-        //'http://localhost:8080' +'/public/service-worker.js'
         navigator.serviceWorker.register('service-worker.js')
             .then( obj => {
                 console.log("serviceWorker register!")
-                console.log("obj:", obj)
+                //console.log("obj:", obj)
             })
             .catch(err => {
                 console.log("serviceWorker err:", err)
