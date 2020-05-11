a7a393b8020a16b7e5a8af68558dff6fb7c9ca80
Aleksey <aleksei.sirmais@yandex.ru>
2020-05-11 18:20:29 +0300

    err

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './index.css'
 import Router from "./utils/router.js"
 import {createOfflinePage} from "./components/OfflinePage/OfflinePage.js"
+import * as navigationPreload from 'workbox-navigation-preload';
+import {registerRoute, NavigationRoute} from 'workbox-routing';
+import {NetworkOnly} from 'workbox-strategies';
+
+
 
 if ('serviceWorker' in navigator) {
     window.addEventListener("load", () => {
@@ -13,10 +18,45 @@ if ('serviceWorker' in navigator) {
                 console.log("serviceWorker err:", err)
             });
 
+
+        const CACHE_NAME = '/'; // добавляется в кеш при загрузки главной
+        const FALLBACK_HTML_URL = '/';
+        self.addEventListener('install', async (event) => {
+            event.waitUntil(
+                caches.open(CACHE_NAME)
+                    .then((cache) => cache.add(FALLBACK_HTML_URL))
+            );
+        });
+
+        navigationPreload.enable();
+
+        const networkOnly = new NetworkOnly();
+        const navigationHandler = async (params) => {
+            try {
+                // Attempt a network request.
+                return await networkOnly.handle(params);
+            } catch (error) {
+                // If it fails, return the cached HTML.
+                return caches.match(FALLBACK_HTML_URL, {
+                    cacheName: CACHE_NAME,
+                });
+            }
+        };
+
+// Register this strategy to handle all navigations.
+        registerRoute(
+            new NavigationRoute(navigationHandler)
+        );
+
+
+
+
         if (!navigator.onLine) {
             console.log("offline load path:", window.location.pathname)
             createOfflinePage(window.location.pathname, document.title);
         }
+
+
 
 
 
