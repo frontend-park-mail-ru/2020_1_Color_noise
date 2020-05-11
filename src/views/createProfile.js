fb4ce828dfb2fe5164329a68b107beb49d46f717
Aleksey <aleksei.sirmais@yandex.ru>
2020-05-11 23:50:24 +0300

    fix sw

--- src/views/createProfile.js
+++ src/views/createProfile.js
@@ -3,6 +3,7 @@ import { createProfile } from "../components/Profile/Profile";
 import { serverLocate } from "../utils/constants";
 import { unSetScroll } from "../components/Desk/Desk.js";
 import Router from "../utils/router";
+import {createOfflinePage} from "../components/OfflinePage/OfflinePage";
 
 export function createProfileView(state) {
     unSetScroll();
@@ -16,7 +17,13 @@ export function createProfileView(state) {
         )
         .then((result) => {
             if (result.status === 200) {
-                createProfile(state.id, state);
+                console.log("!!!2state:", state)
+                if (state == null) {
+                    createProfile()
+                } else {
+                    createProfile(state.id, state);
+                }
+
             }
             else {
                 //createAutorization();
@@ -24,6 +31,6 @@ export function createProfileView(state) {
         })
         .catch(function(error) {
             console.log("CRETE PROFILE ERR:", error.toString());
-            //setError();
+
         });
 }
