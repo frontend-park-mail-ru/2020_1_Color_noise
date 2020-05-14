39bbae3976dbe4373e0b7d07bf44dabe618fb80b
Aleksey <aleksei.sirmais@yandex.ru>
2020-05-14 21:58:16 +0300

    network only for api

--- src/utils/serviceWorkerMy.js
+++ src/utils/serviceWorkerMy.js
@@ -1,66 +1,30 @@
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
-
-import {NavigationRoute, registerRoute} from 'workbox-routing';
-import {CacheFirst} from 'workbox-strategies';
-import {createHandlerBoundToURL, } from 'workbox-precaching';
-
-
+import {CacheFirst, NetworkOnly} from 'workbox-strategies';
 
 workbox.skipWaiting;
 workbox.clientsClaim;
 
+// workbox.precaching.precacheAndRoute(self.__WB_MANIFEST)
 
-// cache name
-workbox.core.setCacheNameDetails({
-    prefix: 'My-awesome-cache',
-    precache: 'precache',
-    runtime: 'runtime',
-});
-
-
-
-
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST)
-
-
-// первый вариант
-// https://developers.google.com/web/tools/workbox/guides/precache-files/webpack
-workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
-
-
-// второй вариант
 workbox.precaching.precacheAndRoute([
     {url: '/', revision: null},
-        {url: '/chats', revision: null}
-    ],
+    {url: '/chats', revision: null},
+    {url: '/notifications', revision: null},
+    {url: '/profile', revision: null},
+    {url: '/chat', revision: null},
+    {url: '/newPins', revision: null},
+    {url: '/main', revision: null},
+]);
 
-
-    {
-   cleanUrls: true,
- });
-
-
-
-
-
-console.log("try to set default route to /")
-// роутинг для SPA
-// https://developers.google.com/web/tools/workbox/modules/workbox-routing
-// This assumes / has been precached.
-const handler = createHandlerBoundToURL('/');
-const navigationRoute = new NavigationRoute(handler);
-workbox.routing.registerRoute(navigationRoute);
-
-
-
-
+// запросы к апи - только через сеть
+workbox.routing.registerRoute(
+    ({url}) => url.pathname.startsWith('/api/'),
+    new NetworkOnly()
+);
 
 // кешируем все
-// запросы страниц chats|notifications|profile|newPin кешируются только при обновлении страницы, где будет их url
-// запросы для страниц, которые не кешировались перезагрузкой должны отдаваться из кеша для SPA
 workbox.routing.registerRoute(
-    new RegExp(/(?:chats|notifications|profile|newPin|\/|main|pin\/[*]|\.(?:png|jpg|jpeg|svg|html|js|css|))|$/),
+    new RegExp(/(?:png|jpg|jpeg|svg|html|js|css|)/),
     new CacheFirst({
         cacheName: 'My-awesome-cache-imgs-css-spa',
         plugins: [
@@ -72,4 +36,5 @@ workbox.routing.registerRoute(
         ]
     })
 );
+
 
