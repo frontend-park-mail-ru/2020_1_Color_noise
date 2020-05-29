ec0ad15bad51f9695d02a3a4aa91a773b074b5ff
Aleksey <aleksei.sirmais@yandex.ru>
2020-05-30 01:16:31 +0300

    change ip, uncommnet worker

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
-export const serverLocate = 'http://89.208.198.192';
-export const serverLocateWebSocket = 'ws://89.208.198.192';
+//export const serverLocate = 'http://89.208.198.192';
+//export const serverLocateWebSocket = 'ws://89.208.198.192';
 export const chatSupportLogin = "Support";
 
-// export const serverLocate = 'https://zinterest.ru';
-// export const serverLocateWebSocket = 'wss://zinterest.ru';
+ export const serverLocate = 'https://zinterest.ru';
+ export const serverLocateWebSocket = 'wss://zinterest.ru';
